467332b4f1f0371e07832e6c67db061fbb116cbd
Kevin Penney <kpenney@gmail.com>
2020-09-21 11:18:52 -0700

    added hamburger

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './App.css'
+import MainHeader from './Components/Index/MainHeader'
 
 function App() {
-   return <div className='App'></div>
+   return (
+      <div className='App'>
+         <MainHeader />
+      </div>
+   )
 }
 
 export default App
---
+++ src/Components/Index/Hamburger.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hamburger = () => {
+   return (
+      <div className='hamburger'>
+         <div className='layers'></div>
+         <div className='layers'></div>
+         <div className='layers'></div>
+      </div>
+   )
+}
+
+export default Hamburger
