41a2cc7cdd9ea5c3d49ab30285859a638f1c84c5
Kevin Penney <kpenney@gmail.com>
2020-09-24 15:42:07 -0700

    level 3 done

--- src/Components/Index/Level3.jsx
+++ src/Components/Index/Level3.jsx
@@ -12,30 +12,40 @@ const Level3 = () => {
 
    function handleScroll() {
       // console.log('s2', document.querySelector('.section2').getBoundingClientRect().top)
-      // console.log('s1', document.querySelector('.section1').getBoundingClientRect().top)
       if (
          document.querySelector('.section5').getBoundingClientRect().top <=
          window.innerHeight * (2 / 3)
       ) {
          document.querySelector('.rotating-background').classList.add('background5')
+         document.querySelector('.menu4').classList.add('menu-item-shaded')
+         document.querySelector('.menu3').classList.remove('menu-item-shaded')
       } else if (
          document.querySelector('.section4').getBoundingClientRect().top <=
          window.innerHeight * (2 / 3)
       ) {
          document.querySelector('.rotating-background').classList.add('background4')
          document.querySelector('.rotating-background').classList.remove('background5')
+         document.querySelector('.menu3').classList.add('menu-item-shaded')
+         document.querySelector('.menu4').classList.remove('menu-item-shaded')
+         document.querySelector('.menu2').classList.remove('menu-item-shaded')
       } else if (
          document.querySelector('.section3').getBoundingClientRect().top <=
          window.innerHeight * (2 / 3)
       ) {
          document.querySelector('.rotating-background').classList.add('background3')
          document.querySelector('.rotating-background').classList.remove('background4')
+         document.querySelector('.menu2').classList.add('menu-item-shaded')
+         document.querySelector('.menu3').classList.remove('menu-item-shaded')
+         document.querySelector('.menu1').classList.remove('menu-item-shaded')
       } else if (
          document.querySelector('.section2').getBoundingClientRect().top <=
          window.innerHeight * (2 / 3)
       ) {
          document.querySelector('.rotating-background').classList.add('background2')
          document.querySelector('.rotating-background').classList.remove('background3')
+         document.querySelector('.menu1').classList.add('menu-item-shaded')
+         document.querySelector('.menu2').classList.remove('menu-item-shaded')
+         document.querySelectorAll('.tech').forEach(node => node.classList.remove('font-green'))
       } else if (
          document.querySelector('.section1').getBoundingClientRect().top <=
          window.innerHeight * (2 / 3)
@@ -44,16 +54,51 @@ const Level3 = () => {
          document.querySelector('.background-color').classList.add('fade-out-white')
          document.querySelector('.rotating-background').classList.remove('background2')
          document.querySelector('.sticky-menu').classList.add('menu-appear')
+         document.querySelector('.menu1').classList.remove('menu-item-shaded')
+         document.querySelectorAll('.tech').forEach(node => node.classList.add('font-green'))
       } else {
          document.querySelector('.background-color').classList.remove('fade-out-white')
+         document.querySelector('.sticky-menu').classList.remove('menu-appear')
          // document.querySelector('.rotating-background').classList.remove('background1')
       }
    }
 
    return (
       <section className='level3'>
+         <menu className='sticky-menu'>
+            <div className='vertical-line'></div>
+            <div className='menu-flexbox font-subtitle'>
+               <div className='menu1 menu-item'>
+                  <FintechSVG />
+                  <p>
+                     FIN<span className='tech'>TECH</span>
+                  </p>
+               </div>
+               <div className='line1 h-line'></div>
+               <div className='menu2 menu-item'>
+                  <RetechSVG />
+                  <p>
+                     RE<span className='tech'>TECH</span>
+                  </p>
+               </div>
+               <div className='line2 h-line'></div>
+               <div className='menu3 menu-item'>
+                  <ExtechSVG />
+                  <p>
+                     EX<span className='tech'>TECH</span>
+                  </p>
+               </div>
+               <div className='line3 h-line'></div>
+               <div className='menu4 menu-item'>
+                  <LifetechSVG />
+                  <p>
+                     LIFE<span className='tech'>TECH</span>
+                  </p>
+               </div>
+            </div>
+         </menu>
          <div className='section1 section'></div>
-         <div className='content'>
+         <div className='content content1'>
             <h2>Growth equity is an operators game.</h2>
             <p className='font-body'>
                Our experience, expertise and history of outperformance are why founders pursue deep
@@ -62,30 +107,6 @@ const Level3 = () => {
                opportunities across the very industries we helped shape.
             </p>
          </div>
-         <menu className='sticky-menu'>
-            <div className='vertical-line'></div>
-            <div className='menu-flexbox font-subtitle'>
-               <div className='menu1 menu-item'>
-                  <FintechSVG />
-                  <p>FINTECH</p>
-               </div>
-               <div className='line1 h-line'></div>
-               <div className='menu2 menu-item'>
-                  <RetechSVG />
-                  <p>RETECH</p>
-               </div>
-               <div className='line2 h-line'></div>
-               <div className='menu3 menu-item'>
-                  <ExtechSVG />
-                  <p>EXTECH</p>
-               </div>
-               <div className='line3 h-line'></div>
-               <div className='menu4 menu-item'>
-                  <LifetechSVG />
-                  <p>LIFETECH</p>
-               </div>
-            </div>
-         </menu>
          <div className='section2'>
             <div className='content'>
                <h3>FINANCIAL TECHNOLOGY</h3>
