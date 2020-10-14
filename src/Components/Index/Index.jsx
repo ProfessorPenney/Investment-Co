9486d1d962c5023052282209a1038b042914858a
Kevin Penney <kpenney@gmail.com>
2020-10-14 10:03:29 -0700

    deleted useless background divs

--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -23,10 +23,6 @@ const Index = () => {
       <React.Fragment>
          <div className='background-color'></div>
          <div className='background1'></div>
-         {/* <div className='background2 bg'></div>
-         <div className='background3 bg'></div>
-         <div className='background4 bg'></div>
-         <div className='background5 bg'></div> */}
          <MainHeader />
          <Level2 />
          <Level3 />
