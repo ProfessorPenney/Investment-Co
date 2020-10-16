c15eb68997ed4bebdf85eb4224efba2850d5d090
Kevin Penney <kpenney@gmail.com>
2020-10-16 10:53:16 -0700

    fixed scroll function so it will look nice when resumed on back button

--- src/Components/Index/scroll.js
+++ src/Components/Index/scroll.js
@@ -4,6 +4,7 @@ const scroll = () => {
       document.querySelector('.level6-begin').getBoundingClientRect().top <=
       window.innerHeight * (1 / 2)
    ) {
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.level6').classList.add('level6-appear')
       document.querySelector('.level5').classList.remove('level5-appear')
       document.querySelector('.background-color').classList.remove('background-color-appear')
@@ -22,6 +23,7 @@ const scroll = () => {
       document.querySelector('.level4').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.level4').classList.add('level4-appear')
       document.querySelector('.background1').classList.remove('background-appear')
       document.querySelector('.level5').classList.remove('level5-appear')
@@ -34,6 +36,9 @@ const scroll = () => {
       document.querySelector('.section5').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.background1').classList.add('background-appear')
+      document.querySelector('.sticky-menu').classList.add('menu-appear')
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.background1').classList.add('background5')
       document.querySelector('.background1').classList.add('background-appear')
       document.querySelector('.background-color').classList.remove('background-color-appear')
@@ -46,6 +51,9 @@ const scroll = () => {
       document.querySelector('.section4').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.background1').classList.add('background-appear')
+      document.querySelector('.sticky-menu').classList.add('menu-appear')
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.background1').classList.add('background4')
       document.querySelector('.background1').classList.remove('background5')
       document.querySelector('.menu3').classList.add('menu-item-shaded')
@@ -55,6 +63,9 @@ const scroll = () => {
       document.querySelector('.section3').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.background1').classList.add('background-appear')
+      document.querySelector('.sticky-menu').classList.add('menu-appear')
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.background1').classList.add('background3')
       document.querySelector('.background1').classList.remove('background4')
       document.querySelector('.menu2').classList.add('menu-item-shaded')
@@ -64,6 +75,9 @@ const scroll = () => {
       document.querySelector('.section2').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.background1').classList.add('background-appear')
+      document.querySelector('.sticky-menu').classList.add('menu-appear')
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.background1').classList.add('background2')
       document.querySelector('.background1').classList.remove('background3')
       document.querySelector('.menu1').classList.add('menu-item-shaded')
@@ -73,23 +87,16 @@ const scroll = () => {
       document.querySelector('.section1').getBoundingClientRect().top <=
       window.innerHeight * (2 / 3)
    ) {
+      document.querySelector('.main-header').classList.add('main-header-disappear')
       document.querySelector('.background1').classList.add('background-appear')
       document.querySelector('.background1').classList.remove('background2')
       document.querySelector('.background-color').classList.remove('background-color-appear')
       document.querySelector('.sticky-menu').classList.add('menu-appear')
       document.querySelector('.menu1').classList.remove('menu-item-shaded')
       document.querySelectorAll('.tech').forEach(node => node.classList.add('font-green'))
-      // document
-      // .querySelector('.investment-container')
-      // .classList.remove('investment-container-appear')
       document.querySelector('.burger').classList.remove('burger-blue')
       document.querySelector('.caption-20years').classList.remove('caption-20years-appear')
       document.querySelector('.svg-container').classList.remove('svg-container-appear')
-      // } else if (
-      //    document.querySelector('.investment-container').getBoundingClientRect().top <=
-      //    window.innerHeight * (2 / 3)
-      // ) {
-      // document.querySelector('.investment-container').classList.add('investment-container-appear')
    } else if (
       document.querySelector('.caption-20years').getBoundingClientRect().top <=
       window.innerHeight * (9 / 10)
@@ -99,9 +106,6 @@ const scroll = () => {
       document.querySelector('.background-color').classList.add('background-color-appear')
       document.querySelector('.burger').classList.add('burger-blue')
       document.querySelector('.caption-20years').classList.add('caption-20years-appear')
-      // document
-      //    .querySelector('.investment-container')
-      //    .classList.remove('investment-container-appear')
       document.querySelector('.svg-container').classList.add('svg-container-appear')
    } else if (
       document.querySelector('.level2').getBoundingClientRect().top <=
