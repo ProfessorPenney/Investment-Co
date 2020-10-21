4dd5296f9d6df94b778929882b05b08432e64ed5
Kevin Penney <kpenney@gmail.com>
2020-10-21 14:26:53 -0700

    deleted old commented out code

--- src/Components/Index/MainHeader.jsx
+++ src/Components/Index/MainHeader.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
-// import React, { useEffect, useRef } from 'react'
-// import { useLocation } from 'react-router-dom'
 import CompassSVG from '../../SVGs/CompassSVG'
 import HeaderLinks from '../UI/HeaderLinks'
 
 const MainHeader = () => {
-   // const myRef = useRef(null)
-   // const { pathname } = useLocation()
-
-   // useEffect(() => {
-   //    if (pathname === '/') {
-   //       window.scrollTo(0, myRef.current.offsetTop)
-   //       console.log(myRef)
-   //    }
-   //    console.log(pathname)
-   // }, [pathname])
-
-   // function animateHero() {
-   //    document.querySelector('.App').classList.add('app-appear')
-   // }
-
    return (
       <header className='main-header'>
-         {/* <div ref={myRef} style={{ height: '100px', backgroundColor: 'red' }}></div> */}
          <div className='hero-background' />
          <div className='line1 line'></div>
          <div className='line2 line'></div>
